f2aec44120a15158638047e5027e26284aea035b
Avishek Gurung <avishek.gurung@myntra.com>
2023-08-07 09:51:54 +0530

    remving lombok and compiler 8

--- src/main/java/practice/LabTest.java
+++ src/main/java/practice/LabTest.java
@@ -1,15 +1,9 @@
 package practice;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.PriorityQueue;
 import java.util.Stack;
 
 public class LabTest {
